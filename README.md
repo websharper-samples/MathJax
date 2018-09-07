9c3e301a141af4fcf9c523a9c65199d583ee4cf5
jooseppi12 <urijozsef@europe.com>
2018-09-07 10:51:11 +0200

    Initial commit for MathJax

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# WebSharper @sampleName@
+# WebSharper MathJax
 
-Sample for [WebSharper](https://websharper.com) bindings for @libLink@.
+Sample for [WebSharper](https://websharper.com) bindings for MathJax.
 
-* [Source repository on GitHub](@repoUrl@)
-* [See it running live](@liveUrl@)
+* [Source repository on GitHub](https://github.com/websharper-samples/MathJax)
+* [See it running live](https://websharper-samples.github.io/MathJax)
--- src/Client.fs
+++ src/Client.fs
@@ -1,1 +1,45 @@
-TODO Replace me
+namespace Samples
+
+open WebSharper
+open WebSharper.JavaScript
+open WebSharper.JQuery
+open WebSharper.UI
+open WebSharper.UI.Client
+open WebSharper.UI.Html
+
+[<JavaScript>]
+module TeXInput =
+    open WebSharper.MathJax
+
+    type MainTemplate = Templating.Template<"wwwroot/index.html">
+
+    [<SPAEntryPoint>]
+    let Main () =
+        MathJax.Hub.Config(
+            MathJax.Config(
+                Extensions = [| "tex2jax.js" |],
+                Jax = [| "input/TeX"; "output/HTML-CSS"; |],
+                Tex2jax = MathJax.Tex2jax(InlineMath = [| ("$", "$"); ("\\(", "\\)") |])
+            )
+        )
+
+        let rvExpression = Var.Create @"\frac{2\cdot x\cdot\left({ x}^{9}+{ x}^{2}\right)-{ x}^{2}\cdot\left(9\cdot{ x}^{8}+2\cdot x\right)}{{\left({ x}^{9}+{ x}^{2}\right)}^{2}}+2\cdot{ x}^{3}"
+
+        let viewExpression = rvExpression.View
+
+        let tex = 
+            div [
+                on.viewUpdate viewExpression (fun e v -> 
+                    JQuery.Of("#tex div").Empty().Append("$$" + v + "$$").Ignore
+                    MathJax.Hub.Queue([| "Typeset", MathJax.MathJax.Hub :> obj, [| e :> obj |] |]) |> ignore
+                )
+            ] [
+                textView <| viewExpression.Map (fun x -> "$$" + x + "$$")
+            ]
+
+        Doc.RunById "tex" tex
+
+        MainTemplate.Main()
+            .Expression(rvExpression)
+            .Doc()
+        |> Doc.RunById "main"
